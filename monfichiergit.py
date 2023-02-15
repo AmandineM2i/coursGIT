9617ae852a08b776f49abe4193b578b3810e852d
Amandine <amande3333@msn.com>
2023-02-15 15:18:04 +0100

    reparation

--- monfichiergit.py
+++ monfichiergit.py
@@ -3,6 +3,8 @@ print("c'est un fichier à sauvegarder dans GIT blabla")
 print("git add . ")                         # pour ajouter les nouveaux fichiers à mon dossier local GIT
 # on peut soit mettre add . → le . fait référence à tous les fichiers de mon dossier de travail
 # on peut soit mettre add NOMFICHIER.py → on envoie qu'un seul fichier qu'on nomme explicitement 
-print('git commit -m "premiere version"')   # pour enregistrer les modifications
+print('git commit -m "premiere version"')   # pour enregistrer les modifications en local
 print("git push -u origin main")            # pour envoyer les modifications à GITHUB
+
+print("git status")                         # pour voir le statut 
 
